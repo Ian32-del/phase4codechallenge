96788dfeea089be743fc34882fd45e208482581f
Ian <ian.odhiambo@student.moringaschool.com>
2023-09-24 10:11:34 +0300

    Made the Validation

--- server/model.py
+++ server/model.py
@@ -1,18 +1,23 @@
-from app import db
+from flask_SQLAlchemy import SQLAlchemy
+from sqlalchemy import CheckConstraint
+
+db = SQLAlchemy
 
 
 restaurant_pizza = db.Table(
     'restaurant_pizza',
     db.Column('restaurant_id', db.Integer, db.ForeignKey('restaurant.id')),
-    db.Column('pizza_id', db.Integer, db.ForeignKey('pizza.id'))
+    db.Column('pizza_id', db.Integer, db.ForeignKey('pizza.id')),
+    db.Column('price', db.Float , nullable=False),
+    CheckConstraint('price >= 1 AND price <= 30', name='check_price_range')
 )
 
 class Restaurant(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(100))
+    name = db.Column(db.String(100), unique=True, nullable=False)
     pizzas = db.relationship('Pizza', secondary=restaurant_pizza, back_populates='restaurants')
 
 class Pizza(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(100))
+    name = db.Column(db.String(100) , nullable=False)
     restaurants = db.relationship('Restaurant', secondary=restaurant_pizza, back_populates='pizzas')
