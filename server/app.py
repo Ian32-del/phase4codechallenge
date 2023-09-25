e943f72d0e107658a787e55a077e95b385c03c73
Ian <ian.odhiambo@student.moringaschool.com>
2023-09-25 13:37:03 +0300

    Get done

--- server/app.py
+++ server/app.py
@@ -1,7 +1,8 @@
-from flask import Flask
+from flask import Flask , jsonify , request
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from model import Restaurant, Pizza, RestaurantPizza
+import json
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///pizza_restaurant.db'
@@ -10,11 +11,27 @@ app.config['SQLALCHEMY_TRACK-MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 
+# Read data from the JSON seed file
+with open('seed.py', 'r') as json_file:
+    data = json.load(json_file)
+
 
 
 @app.route('/')
 def index():
     return '<hi> Welcome </h1>'
+
+@app.route('/restaurants' , methods=['GET'])
+def get_restaurants():
+    return jsonify(data['restaurants'])
+
+@app.route('/restaurant/<int:id>', methods=['GET'])
+def get_restaurant(id):
+    restaurant = next((r for r in data['restaurants'] if r['id'] == id), None)
+    if restaurant:
+        return jsonify(restaurant)
+    else:
+        return jsonify({'error': 'Restaurant not found'}), 404
 
 if __name__ == '__main__':
     app.run(port=5555,debug=True)
---
+++ server/seed.py
@@ -0,0 +1,26 @@
+{
+  "restaurants": [
+    {
+      "id": 1,
+      "name": "Dominion Pizza",
+      "address": "Good Italian, Ngong Road, 5th Avenue"
+    },
+    {
+      "id": 2,
+      "name": "Pizza Hut",
+      "address": "Westgate Mall, Mwanzi Road, Nrb 100"
+    }
+  ],
+  "pizzas": [
+    {
+      "id": 1,
+      "name": "Cheese",
+      "ingredients": "Dough, Tomato Sauce, Cheese"
+    },
+    {
+      "id": 2,
+      "name": "Pepperoni",
+      "ingredients": "Dough, Tomato Sauce, Cheese, Pepperoni"
+    }
+  ]
+}
